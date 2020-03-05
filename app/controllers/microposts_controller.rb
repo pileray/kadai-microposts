6190eef0a49f7cf007caed8b504eb65fb33a81fb
ryosuke.fukui <fukuiryo.0312@gmail.com>
2020-03-05 08:50:52 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,14 +2,13 @@ class MicropostsController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
   
-  
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = 'メッセージを投稿しました'
       redirect_to root_url
     else
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page]).per(3)
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました'
       render 'toppages/index'
     end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,7 +3,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @micropost = current_user.microposts.build
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page]).per(3)
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
     end
   end
   
